0381d0f3729a1b26b7849f4c50a57f0ef78d0aba
shivanshverma123 <shivanshverma123@gmail.com>
2024-09-24 10:10:38 +0530

    Delete some.py

--- some.py
+++
@@ -1,6 +0,0 @@
-sg-col-20-of-24 s-result-item s-asin sg-col-0-of-12 sg-col-16-of-20 sg-col s-widget-spacing-small sg-col-12-of-16
-
-
-sg-col-20-of-24 s-result-item s-asin sg-col-0-of-12 sg-col-16-of-20 sg-col s-widget-spacing-small sg-col-12-of-16
-
-sg-col-4-of-24 sg-col-4-of-12 s-result-item sg-col-4-of-16 sg-col sg-col-4-of-20
